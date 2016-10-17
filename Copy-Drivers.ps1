869b7e30d9bbbf9d0c8b217f2e63ac17e6d7224e
Atamido <atamido+github@gmail.com>
2016-10-16 20:44:13 -0500

    some message

---
+++ Copy-Drivers.ps1
@@ -0,0 +1,63 @@
+﻿#  Collect OEM drivers the OS is using for attached hardware, and copy them out so that they can be inserted into a driver package
+
+
+#  Set up destination directory for drivers
+$DriverRoot = 'C:\Temp\DriverCollection'
+$DateTime = Get-Date -Format yyyyMMddhhmmss
+$DriverFolder = "$($DriverRoot)\$($DateTime)"
+If (!(Test-Path -Path $DriverFolder -PathType Container))
+{
+    New-Item -Path $DriverRoot -Name $DateTime -ItemType Directory -Force | Out-Null
+}
+If (!(Test-Path -Path "$($DriverRoot)\Copy-Drivers.ps1" -PathType Leaf))
+{
+    Copy-Item -Path $PSCommandPath -Destination $DriverRoot -Force
+}
+Write-Host "Working directory: $($DriverFolder)"
+"Working directory: $($DriverFolder)" | Out-File -FilePath "$($DriverFolder)\DriverCollectionLog.txt" -Append
+
+#  A little information about the system
+gwmi win32_computersystem | Format-List | Out-File -FilePath "$($DriverFolder)\DriverCollectionLog.txt" -Append
+
+#  Get list of drivers being used for attached hardware
+$PnPSignedDrivers = @(Get-WmiObject Win32_PnPSignedDriver)
+$OEMPnPSignedDrivers = @($PnPSignedDrivers | Where-Object {$_.InfName -like 'oem*'})
+[string[]]$OEMDrivers = $OEMPnPSignedDrivers | Select-Object -ExpandProperty InfName | Sort-Object -Unique
+Write-Host "$($OEMDrivers.Count) OEM drivers in use by $($OEMPnPSignedDrivers.Count) devices"
+"$($OEMDrivers.Count) OEM drivers in use by $($OEMPnPSignedDrivers.Count) devices" | Out-File -FilePath "$($DriverFolder)\DriverCollectionLog.txt" -Append
+
+#  Get list of drivers installed on the system, which includes where the driver files are located
+$WindowsDrivers = @(Get-WindowsDriver -Online | Where-Object {$_.Driver -like 'oem*'})
+$OriginalFileNames = @($WindowsDrivers | Where-Object {$OEMDrivers -contains $_.Driver} | Select-Object -ExpandProperty OriginalFileName | Sort-Object -Unique)
+Write-Host "$($WindowsDrivers.Count) OEM drivers installed"
+"$($WindowsDrivers.Count) OEM drivers installed" | Out-File -FilePath "$($DriverFolder)\DriverCollectionLog.txt" -Append
+
+#  Create list of drivers that have already been copied to the driver folder so only new drivers are copied during this run of the script
+[String[]]$Copied = Get-ChildItem -Path "$($DriverRoot)\*\*" -Directory | Select-Object -ExpandProperty Name | Sort-Object -Unique
+Write-Host "$($Copied.Count) drivers already copied"
+"$($Copied.Count) drivers already copied" | Out-File -FilePath "$($DriverFolder)\DriverCollectionLog.txt" -Append
+
+#  Associage the drivers being used by hardware with the driver file locations, and copy those files out
+$Count = 0
+ForEach ($OriginalFileName in $OriginalFileNames) {
+    $OEMFolder = (Get-Item -Path $OriginalFileName).Directory
+    If ($Copied -notcontains $OEMFolder.Name)
+    {
+        Copy-Item -Path $OEMFolder.FullName -Destination $DriverFolder -Recurse
+        $Count++
+    }
+}
+Write-Host "$($Count) new drivers copied"
+"$($Count) new drivers copied" | Out-File -FilePath "$($DriverFolder)\DriverCollectionLog.txt" -Append
+
+#  Output a pretty table of OEM drivers
+$OEMHash = @{}
+$WindowsDrivers | ForEach {$OEMHash[$_.Driver] = $_}
+$OEMPnPSignedDrivers | Sort-Object -Property devicename | ForEach {Add-Member -InputObject $_ -MemberType NoteProperty -Name 'OriginalFileName' -Value ($OEMHash[$_.InfName].OriginalFileName -replace '(.*\\)(.*?)(\\.*)','$2')}
+$OEMPnPSignedDrivers | Sort-Object -Property devicename | ForEach {Add-Member -InputObject $_ -MemberType NoteProperty -Name 'Date' -Value ($_.DriverDate.Substring(0,8))}
+$OEMPnPSignedDrivers | Sort-Object -Property devicename | Format-Table -Property OriginalFileName,DeviceName,DriverVersion,Date,DriverProviderName,InfName -AutoSize | Out-File -FilePath "$($DriverFolder)\DriverCollectionLog.txt" -Append
+
+#  We don't want these files in the Driver Packages as they're extras, so delete them now
+Remove-Item -Path $DriverRoot -Include *.pnf -Recurse
+
+
---
+++ Hide-Window.ps1
@@ -0,0 +1,121 @@
+﻿
+Function Hide-Window {
+    Param(
+        [Parameter(Position=0,Mandatory=$true)][String]$Title,
+        [Parameter(Position=1)][int]$WindowAction=0
+    )
+
+    #  Valid values for WindowAction can be found on the MSDN documentation for the ShowWindow function
+    #  https://msdn.microsoft.com/en-us/library/ms633548%28VS.85%29.aspx
+
+    Add-Type -TypeDefinition @'
+        using System;
+        using System.Collections;
+        using System.Collections.Generic;
+        using System.Runtime.InteropServices;
+        using System.Text;
+        using Microsoft.Win32;
+        namespace WindowFunctions {
+            public class Setter {
+
+
+                [DllImport("user32.dll", CharSet = CharSet.Unicode)]
+                private static extern int GetWindowText(IntPtr hWnd, StringBuilder strText, int maxCount);
+
+                [DllImport("user32.dll", CharSet = CharSet.Unicode)]
+                private static extern int GetWindowTextLength(IntPtr hWnd);
+
+                [DllImport("user32.dll")]
+                private static extern bool EnumWindows(EnumWindowsProc enumProc, IntPtr lParam);
+
+                [DllImport("user32.dll")]
+                private static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+                // Delegate to filter which windows to include 
+                public delegate bool EnumWindowsProc(IntPtr hWnd, IntPtr lParam);
+
+                /// <summary> Get the text for the window pointed to by hWnd </summary>
+                public static string GetMyWindowText(IntPtr hWnd)
+                {
+                    int size = GetWindowTextLength(hWnd);
+                    if (size > 0)
+                    {
+                        var builder = new StringBuilder(size + 1);
+                        GetWindowText(hWnd, builder, builder.Capacity);
+                        return builder.ToString();
+                    }
+
+                    return String.Empty;
+                }
+
+                /// <summary> Find all windows that match the given filter </summary>
+                /// <param name="filter"> A delegate that returns true for windows
+                ///    that should be returned and false for windows that should
+                ///    not be returned </param>
+                public static IEnumerable<IntPtr> FindWindows(EnumWindowsProc filter)
+                {
+                    IntPtr found = IntPtr.Zero;
+                    List<IntPtr> windows = new List<IntPtr>();
+
+                    EnumWindows(delegate(IntPtr wnd, IntPtr param)
+                    {
+                        if (filter(wnd, param))
+                        {
+                            // only add the windows that pass the filter
+                            windows.Add(wnd);
+                        }
+
+                        // but return true here so that we iterate all windows
+                        return true;
+                    }, IntPtr.Zero);
+
+                    return windows;
+                }
+
+                /// <summary> Find all windows that contain the given title text </summary>
+                /// <param name="titleText"> The text that the window title must contain. </param>
+                public static IEnumerable<IntPtr> FindWindowsWithText(string titleText)
+                {
+                    return FindWindows(delegate(IntPtr wnd, IntPtr param)
+                    {
+                        return GetMyWindowText(wnd).Contains(titleText);
+                    });
+                }
+
+                /// <summary> Count all windows that contain the given title text </summary>
+                /// <param name="titleText"> The text that the window title must contain. </param>
+                public static Int32 CountWindowsWithText(string titleText)
+                {
+                    List<IntPtr> cList = new List<IntPtr>();
+                    IEnumerable<IntPtr> windows = FindWindowsWithText(titleText);
+                    cList.AddRange(windows);
+                    return cList.Count;
+                }
+
+                /// <summary> Count all windows that contain the given title text </summary>
+                /// <param name="titleText"> The text that the window title must contain. </param>
+                public static Int32 HideWindowsWithText(string titleText, int nCmdShow)
+                {
+                    List<IntPtr> cList = new List<IntPtr>();
+                    IEnumerable<IntPtr> windows = FindWindowsWithText(titleText);
+                    cList.AddRange(windows);
+                    if(cList.Count > 0)
+                    {
+                        foreach (IntPtr window in windows)
+                        {
+                            ShowWindow(window, nCmdShow);
+                        }
+                    }
+                    return cList.Count;
+                }
+            }
+        }
+'@
+
+
+    [WindowFunctions.Setter]::HideWindowsWithText($Title, $WindowAction)
+}
+
+$windows = Hide-Window -Title 'FirstUXWnd' -WindowAction 0
+Write-Host "Hid $($windows) windows"
+
